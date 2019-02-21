5304520cb3e810b4381f0ee55871dee6c175e2e6
Premasagar Mulage <35753805+premsagarm@users.noreply.github.com>
2019-02-21 12:18:12 +0530

    Add files via upload

---
+++ Exp1.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<string.h>
+#define N strlen(g)
+void crc();
+void xor();
+char g[20];
+char text[100], code[100];
+int len;
+int main()
+{
+int i, flag=0;
+printf("Enter the message in binary form");
+scanf("%s",text);
+printf("Enter the generated number");
+scanf("%s",g);
+len=strlen(text);
+for (i=len;i<len+N-1; i++)
+{
+text[i]=0;
+}
+crc();
+for(i=len; i<len+N-1; i++)
+{
+text[i] = code [i-len];
+}
+printf("\n Final code word is : %s \n", text);
+printf("\n Enter coded word:");
+scanf("%s",text);
+crc();
+for (i=0;i<strlen(code);i++)
+{
+if (code[i]== 1)
+{
+flag=1;
+break;
+}
+}
+if (flag==1)
+{
+printf("error");
+}
+else
+{
+printf("No error");
+}
+return 0;
+}
+void crc()
+{
+int i,j;
+for (i=0;i<N;i++)
+{
+code[i]=text[i];
+}
+do {
+if(code[0]==1)
+{
+xor();
+}
+for(j=0;j<N-1;j++)
+{
+code[j]=code[j+1];
+}
+code[j]=text[i];
+i++;
+
+}while(i<=len+N-1);
+}
+void xor()
+{
+int i;
+for (i=0;i<N;i++)
+{
+if(code[i]==g[i])
+{
+code[i]=0;
+}
+else
+code[i]=1;
+}
+}
